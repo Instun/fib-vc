80957368aad391270076f515395dbbb2f5c449b1
xicilion <lion@9465.net>
2023-11-18 17:36:56 +0800

    v1.1.0.

--- test/index.js
+++ test/index.js
@@ -6,19 +6,20 @@ const path = require('path');
 const vc = require("../lib");
 
 const keys = fs.readdir(path.join(__dirname, "./data/keys")).map(key => path.basename(key, ".json"));
-const creds = fs.readdir(path.join(__dirname, "./data/credentials")).map(cred => path.basename(cred, ".json"));
+const credentials = fs.readdir(path.join(__dirname, "./data/credentials")).map(credential => path.basename(credential, ".json"));
+const presentations = fs.readdir(path.join(__dirname, "./data/presentations")).map(presentation => path.basename(presentation, ".json"));
 const impls = fs.readdir(path.join(__dirname, "./data/implementations"));
 
 describe("vc", () => {
     describe("issue/verify", () => {
-        creds.forEach(cred => {
-            const credJson = require(`./data/credentials/${cred}.json`);
+        credentials.forEach(credential => {
+            const credentialJson = require(`./data/credentials/${credential}.json`);
             keys.forEach(key => {
                 const keyJson = require(`./data/keys/${key}.json`);
-                it(`${cred}--${key}`, () => {
+                it(`${credential}--${key}`, () => {
                     var vcJson = vc.credential.issue({
-                        credential: credJson,
-                        date: credJson.issuanceDate,
+                        credential: credentialJson,
+                        date: credentialJson.issuanceDate,
                         id: keyJson.id,
                         key: keyJson.privateKeyJwk
                     });
@@ -45,13 +46,13 @@ describe("vc", () => {
     describe("implementations", () => {
         impls.forEach(impl => {
             describe(`${impl}`, () => {
-                creds.forEach(cred => {
-                    const credJson = require(`./data/credentials/${cred}.json`);
+                credentials.forEach(credential => {
+                    const credentialJson = require(`./data/credentials/${credential}.json`);
                     keys.forEach(key => {
                         const keyJson = require(`./data/keys/${key}.json`);
-                        if (fs.exists(path.join(__dirname, `./data/implementations/${impl}/${cred}--${key}.vc.json`)))
-                            it(`${cred}--${key}`, () => {
-                                const vcJson = require(`./data/implementations/${impl}/${cred}--${key}.vc.json`);
+                        if (fs.exists(path.join(__dirname, `./data/implementations/${impl}/${credential}--${key}.vc.json`)))
+                            it(`${credential}--${key}`, () => {
+                                const vcJson = require(`./data/implementations/${impl}/${credential}--${key}.vc.json`);
 
                                 var r = vc.credential.verify({
                                     credential: vcJson,
@@ -74,5 +75,69 @@ describe("vc", () => {
     });
 });
 
+describe("vp", () => {
+    describe("issue/verify", () => {
+        presentations.forEach(presentation => {
+            const presentationJson = require(`./data/presentations/${presentation}.json`);
+            keys.forEach(key => {
+                const keyJson = require(`./data/keys/${key}.json`);
+                it(`${presentation}--${key}`, () => {
+                    var vcJson = vc.presentation.issue({
+                        presentation: presentationJson,
+                        date: presentationJson.issuanceDate,
+                        id: keyJson.id,
+                        key: keyJson.privateKeyJwk
+                    });
+
+                    var r = vc.presentation.verify({
+                        presentation: vcJson,
+                        key: keyJson
+                    });
+
+                    assert.isTrue(r.verified);
+
+                    vcJson.fake = "fake";
+                    var r = vc.presentation.verify({
+                        presentation: vcJson,
+                        key: keyJson
+                    });
+                    delete vcJson.fake;
+                    assert.equal(r.verified, false);
+                });
+            });
+        });
+    });
+
+    describe("implementations", () => {
+        impls.forEach(impl => {
+            describe(`${impl}`, () => {
+                presentations.forEach(presentation => {
+                    const presentationJson = require(`./data/presentations/${presentation}.json`);
+                    keys.forEach(key => {
+                        const keyJson = require(`./data/keys/${key}.json`);
+                        if (fs.exists(path.join(__dirname, `./data/implementations/${impl}/${presentation}--${key}.vp.json`)))
+                            it(`${presentation}--${key}`, () => {
+                                const vcJson = require(`./data/implementations/${impl}/${presentation}--${key}.vp.json`);
+
+                                var r = vc.presentation.verify({
+                                    presentation: vcJson,
+                                    key: keyJson
+                                });
+                                assert.isTrue(r.verified);
+
+                                vcJson.fake = "fake";
+                                var r = vc.presentation.verify({
+                                    presentation: vcJson,
+                                    key: keyJson
+                                });
+                                delete vcJson.fake;
+                                assert.equal(r.verified, false);
+                            });
+                    });
+                });
+            });
+        });
+    });
+});
 
 test.run(console.DEBUG);
